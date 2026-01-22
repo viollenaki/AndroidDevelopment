8911c4e56109796388250b9ef3e07cf3059fe7e5
viollenaki <tanakaakuustik@gmail.com>
2026-01-22 15:14:51 +0600

    Обновление конфигурации Gradle

--- app/src/main/java/com/example/mbank/MainActivity.kt
+++ app/src/main/java/com/example/mbank/MainActivity.kt
@@ -1,27 +1,39 @@
 package com.example.mbank
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.mbank.ui.theme.MBankTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mbank.ui.theme.ClassesTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MBankTheme {
+            ClassesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android Pro",
+                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -32,16 +44,76 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    var count by remember { mutableStateOf(0) }
+    var count2 by remember {mutableStateOf(0)}
+    var count3 by remember {mutableStateOf(0)}
+    var lastClicked by remember { mutableStateOf("None") }
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        // Используем имя
+        Text(text = "Hello $name!", modifier = Modifier.padding(8.dp))
+
+        Row{
+            Text("Count 1: $count", modifier = Modifier.padding(16.dp))
+            Text("Count 2: $count2", modifier = Modifier.padding(16.dp))
+            Text("Count 3: $count3", modifier = Modifier.padding(16.dp))
+        }
+        Row{
+            Button(
+                onClick = {
+                    count++
+                    lastClicked = "Button 1"
+                },
+                modifier = Modifier.padding(8.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Blue
+                )
+            ) {
+                Text("Button 1")
+            }
+            Button(
+                onClick = {
+                    count2++
+                    lastClicked = "Button 2"
+                },
+                modifier = Modifier.padding(8.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Green
+                )
+            ) {
+                Text("Button 2")
+            }
+            Button(
+                onClick = {
+                    count3++
+                    lastClicked = "Button 3"
+                },
+                modifier = Modifier.padding(8.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Magenta
+                )
+            ) {
+                Text("Button 3")
+            }
+
+        }
+        Text(
+            text = "Last clicked: $lastClicked",
+            modifier = Modifier.padding(16.dp),
+            fontSize = 20.sp,
+            color = Color.Red
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MBankTheme {
+    ClassesTheme {
         Greeting("Android")
     }
 }
--- app/src/main/java/com/example/mbank/ui/theme/Theme.kt
+++ app/src/main/java/com/example/mbank/ui/theme/Theme.kt
@@ -1,8 +1,8 @@
 package com.example.mbank.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -10,6 +10,13 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import com.example.mbank.ui.theme.Pink40
+import com.example.mbank.ui.theme.Pink80
+import com.example.mbank.ui.theme.Purple40
+import com.example.mbank.ui.theme.Purple80
+import com.example.mbank.ui.theme.PurpleGrey40
+import com.example.mbank.ui.theme.PurpleGrey80
+import com.example.mbank.ui.theme.Typography
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -17,24 +24,25 @@ private val DarkColorScheme = darkColorScheme(
     tertiary = Pink80
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+private val LightColorScheme: ColorScheme
+    get() = lightColorScheme(
+        primary = Purple40,
+        secondary = PurpleGrey40,
+        tertiary = Pink40
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+        /* Other default colors to override
+        background = Color(0xFFFFFBFE),
+        surface = Color(0xFFFFFBFE),
+        onPrimary = Color.White,
+        onSecondary = Color.White,
+        onTertiary = Color.White,
+        onBackground = Color(0xFF1C1B1F),
+        onSurface = Color(0xFF1C1B1F),
+        */
+    )
 
 @Composable
-fun MBankTheme(
+fun ClassesTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
